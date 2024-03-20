8f25a0907aa4a6f07af8b1a91ab7b2eead2eb7a1
Layne <laynemcintosh12@gmail.com>
2024-03-20 13:23:49 -0400

    all done

--- db.js
+++ db.js
@@ -4,25 +4,16 @@ const { getDatabaseUri } = require("./config");
 let db;
 
 if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    user: 'laynemcintosh12',
-    host: 'localhost',
-    database: getDatabaseUri(),
-    password: 'password', 
-    port: 5432,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+  db = new Client(
+    getDatabaseUri()
+  );
 } else {
-  db = new Client({
-    user: 'laynemcintosh12',
-    host: 'localhost',
-    database: getDatabaseUri(),
-    password: 'password', 
-    port: 5432
-  });
+  db = new Client(
+    getDatabaseUri(),
+  );
+  
 }
+
 
 db.connect();
 
