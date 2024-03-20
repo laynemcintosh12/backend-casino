8ab8bdd9b9469832c9cadc6f9157d308f72c87ed
Layne <laynemcintosh12@gmail.com>
2024-03-20 13:22:00 -0400

    all done

--- config.js
+++ config.js
@@ -2,14 +2,15 @@ require("dotenv").config();
 require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+const { dbURI } = require "./secret";
 
 const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "postgres://krkfqxaf:n-wbtGfYyKuAMuG57ozDATrHeoYGf4Kl@mahmud.db.elephantsql.com/krkfqxaf"
-      : process.env.DATABASE_URL || "postgres://krkfqxaf:n-wbtGfYyKuAMuG57ozDATrHeoYGf4Kl@mahmud.db.elephantsql.com/krkfqxaf";
+      ? dbURI
+      : process.env.DATABASE_URL || dbURI;
 }
 
 
---
+++ secret.js
@@ -0,0 +1,5 @@
+const dbURI = "postgres://krkfqxaf:n-wbtGfYyKuAMuG57ozDATrHeoYGf4Kl@mahmud.db.elephantsql.com/krkfqxaf"
+
+module.exports = {
+    dbURI
+}
